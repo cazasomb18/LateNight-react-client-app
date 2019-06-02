b4e30222a7ab09f941b327f9b5f52199827e950e
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-06-02 13:33:39 -0500

    restored login with react client data

--- src/Header/index.js
+++ src/Header/index.js
@@ -8,7 +8,6 @@ const Header = () => {
 			<div className="header header-expand-lg header-dark bg-dark">
 				<Link to='/register'><h2 className=" register register-expand-md register-light bg-dark">Register</h2> </Link> <br/>
 				<Link to='/login'><h2 className=" login login-expand-md login-light bg-dark">Login / Logout</h2> </Link> <br/>
-				}
 			</div>
 		</header>
 
