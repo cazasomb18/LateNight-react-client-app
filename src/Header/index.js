79d24aad0440ce49a69b6b301ff3d90e47bf8f67
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-06-10 05:24:51 -0500

    additional styling added, user stories updated

--- src/Header/index.js
+++ src/Header/index.js
@@ -78,51 +78,48 @@ class Header extends React.Component {
 	    	<div className="form">
 			  <Collapsible className="title" trigger="ABOUT LATE NIGHT BYTES">
 			  	<h3 className="title">What is Late Night Bytes?</h3>
-			  	<p className="subTitle">Late Night Bytes consumes google maps and places api to find late night food in your area.</p>
-			  	<p className="subTitle">Create an account by clicking the 'REGISTER' drop down menu and you will be logged in. </p>
-			  	<p className="subTitle">After you're logged in, try the 'FIND LATE BYTES' button, it will <br/>show you all the restaurants in your area!</p>
-			  	<p className="subTitle">If you see something you like on the list, go ahead and leave a comment, <br/>this information will be stored in your dashboard. </p>
-			  	<p className="subTitle">Your dashboard is your private information, and no one else can access it.  <br/>This a good way to keep track of your favorite late night bytes! </p>
+			  	<p className="userStory">Late Night Bytes consumes google maps and places api to find late night food in your area.</p>
+			  	<p className="userStory">Make sure you agree to share your location, Late Night Bytes is dependent up on this to <br/> return the restaurants that match your location, sound good?  GREAT! </p>
+			  	<p className="userStory">Create an account by clicking the 'REGISTER' drop down menu and you will be logged in. </p>
+			  	<p className="userStory">After you're logged in, try the 'FIND LATE BYTES' button, it will <br/>show you all the restaurants in your area!</p>
+			  	<p className="userStory">If you see something you like on the list, go ahead and leave a comment, <br/>this information will be stored in your dashboard. </p>
+			  	<p className="userStory">Your dashboard is your private information, and no one else can access it.  <br/>This a good way to keep track of your favorite late night bytes! </p>
 			  </Collapsible>
 
-		   	{ 
-		   		this.props.loggedIn
-		   		? 
-		      	<div className="form">
-		      		<h4 className="title">Welcome back {this.state.userName}!!!</h4>
-		      		<button className="field" onClick={this.logOut}>Logout!</button>
-		      	</div>
-		   		:
+	   	{ 
+	   		this.props.loggedIn
+	   		? 
+	      	<div className="form">
+	      		<h4 className="title">Welcome back {this.state.userName}!!!</h4>
+	      		<button className="field" onClick={this.logOut}>Logout!</button>
+	      	</div>
+	   		:
 
-			   	<div className="form">    				
-		    		<div className="form">
-				      <Collapsible className="title" trigger="REGISTRATION">
-								<h1 className="title">Register for LateNight Bytes</h1><br/>
-								<h3 className="subTitle">CREATE AN ACCOUNT!</h3>
-								<form className="form" onSubmit={this.handleRegister}>
-									<input className="field" type="text" name="userName" placeholder="username" onChange={this.handleChange}/><br/>
-									<input className="field" type="password" name="password" placeholder="password" onChange={this.handleChange}/><br/>
-									<input className="field" type="email" name="email" placeholder="email" onChange={this.handleChange}/><br/>
-									<input className="field" type="submit" value="Register!"/>
-								</form>
-
-				      </Collapsible>
-					<div>
+		   	<div className="form">    				
+		      <Collapsible className="title" trigger="REGISTRATION">
+						<h1 className="title">Register for LateNight Bytes</h1><br/>
+						<h3 className="subTitle">CREATE AN ACCOUNT!</h3>
+						<form className="form" onSubmit={this.handleRegister}>
+							<input className="field" type="text" name="userName" placeholder="username" onChange={this.handleChange}/><br/>
+							<input className="field" type="password" name="password" placeholder="password" onChange={this.handleChange}/><br/>
+							<input className="field" type="email" name="email" placeholder="email" onChange={this.handleChange}/><br/>
+							<input className="field" type="submit" value="Register!"/>
+						</form>
+		      </Collapsible>
+			<div>
 
 
-		    		</div>
-				      <Collapsible className="title" trigger="LOGIN/LOGOUT">	
-						      <h1 className="title">Login for LateNight Bytes</h1><br/>
-						      <h3 className="subTitle">LOGIN TO LATENIGHTBYTES!</h3>
-						        <form className="form" onSubmit={this.handleLogin}>
-						          <input className="field" type="text" name="userName" placeholder="username" onChange={this.handleChange}/><br/>
-						          <input className="field" type="password" name="password" placeholder="password" onChange={this.handleChange}/><br/>
-						          <input className="field" type="submit" value="Login!"/>
-						        </form>
-				      </Collapsible>
-					</div>
-
-				</div>
+    		</div>
+		      <Collapsible className="title" trigger="LOGIN/LOGOUT">	
+				      <h1 className="title">Login for LateNight Bytes</h1><br/>
+				      <h3 className="subTitle">LOGIN TO LATENIGHTBYTES!</h3>
+				        <form className="form" onSubmit={this.handleLogin}>
+				          <input className="field" type="text" name="userName" placeholder="username" onChange={this.handleChange}/><br/>
+				          <input className="field" type="password" name="password" placeholder="password" onChange={this.handleChange}/><br/>
+				          <input className="field" type="submit" value="Login!"/>
+				        </form>
+		      </Collapsible>
+		</div>
 		   	}
 
 
